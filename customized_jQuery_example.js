3151259243372c2d29b5388b7d2d5fffd5aae1f2
Xin Wang <xin@xwerx.com>
2014-12-16 16:22:48 +0000

    first commit

---
+++ customized_jQuery_example.js
@@ -0,0 +1,51 @@
+var jQuery, $;
+
+(function() {
+
+    jQuery = $ = function(sel) {
+        return new MyQuery(sel);
+    };
+
+    var MyQuery = function(sel) {
+   
+        var i, l
+        
+        selAction = {
+            'default': document.getElementsByTagName(sel),
+            '#': [ document.getElementById(sel.slice(1)) ],
+            '.': document.getElementsByClassName(sel.slice(1)),
+        },
+            
+        nodes = selAction[sel.charAt(0)] || selAction.default;
+        
+        for (i = 0, l = nodes.length ; i < l; i++) { this[i] = nodes[i]; }
+        this.length = nodes.length;
+        return this;
+    };
+
+    jQuery.fn = MyQuery.prototype = {
+        hide: function() {
+            for (var i = 0, l = this.length; i < l; i += 1) { this[i].style.display = 'none'; }
+            return this;
+        },
+        remove: function() {
+            for (var i = 0, l = this.length; i < l; i += 1) { this[i].parentNode.removeChild( this[i] ); }
+            return this;
+        }
+    };
+}());
+
+$('p').hide().remove();
+
+$.fn.css = function(obj) {
+    var i, j, l
+    if(!obj) return this;
+    for (i = 0, l = this.length; i < l; i += 1) { 
+        for(j in obj){
+            if (obj.hasOwnProperty(j)) this[i].style[j] = obj[j]; 
+        }
+    }
+    return this;
+}
+
+$('.test-class').css({color: 'red'});
